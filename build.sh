96e6784eb361f0172a0d39d55c8772bd36945259
lusyoe <lusyoe@163.com>
2017-02-09 15:08:27 +0800

    跳转docker文件位置修复打包错误，跳过打包测试

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 source /etc/profile
 
-mvn package -e -X docker:build
+docker login -u admin -p admin123
+
+mvn package -e -X docker:build -DskipTest
 
 docker run -dp 9090:8080 springboot-jpa-0.0.1
 
