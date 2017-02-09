d8ff3674ac932060af72b07fc991551ed0134021
lusyoe <lusyoe@163.com>
2017-02-09 15:23:04 +0800

    fix run container error

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ docker login 192.168.1.2:8082 -u admin -p admin123
 
 mvn package -e -X docker:build -DskipTest
 
-docker run -dp 9090:8080 springboot-jpa-0.0.1
+docker run -dp 9090:8080 192.168.1.2:8082/springboot/springboot-jpa
 
