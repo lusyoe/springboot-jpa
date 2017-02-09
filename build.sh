d67fd921324dc23df08f2c9d61957887c894d760
lusyoe <lusyoe@163.com>
2017-02-09 15:41:56 +0800

    remove container stop

--- build.sh
+++ build.sh
@@ -8,8 +8,6 @@ docker login 192.168.1.2:8082 -u admin -p admin123
 
 mvn package -e -X docker:build -DskipTest
 
-docker stop ${MYIMAGE}
-
 docker run -dp 9090:8080 --name springboot-jpa ${IMAGE}
 
 
