ca3541d5a52de9e3c9f7cbdc4db1a58e48e0e3da
lusyoe <lusyoe@163.com>
2017-02-09 16:49:50 +0800

    add remove old container and image

--- build.sh
+++ build.sh
@@ -4,10 +4,22 @@ source /etc/profile
 
 MYIMAGE=192.168.1.2:8082/springboot/springboot-jpa
 
-docker login 192.168.1.2:8082 -u admin -p admin123
+# if push image then uncomment
+#docker login 192.168.1.2:8082 -u admin -p admin123
 
+# stop all container
+docker kill $(docker ps -aq)
+
+# remove all container
+docker rm $(docker ps -aq)
+
+# remove old images
+docker images | grep 192.168.1.2:8082/springboot/springboot-jpa | awk '{print $3}' | xargs docker rmi
+
+# build jar and image
 mvn package -e -X docker:build -DskipTest
 
+# running container
 docker run -dp 8080:8080 --name springboot-jpa ${MYIMAGE}
 
 
