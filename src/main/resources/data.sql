071ef50ba9e770823599dca97e43b56ec29d747f
lusyoe <lusyoe@163.com>
2017-02-09 16:14:06 +0800

    change database to test, add table schema and fill data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO person(NAME, age, address) VALUES ('lu', 25, 'shenzhen'), ('ming', 26, 'caohe');
+
+-- Spring Boot auto insert table data
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE person(
+id      INT PRIMARY KEY AUTO_INCREMENT,
+name    VARCHAR(20),
+age     INT,
+address VARCHAR(40)
+);
+
+-- Spring Boot auto init table struct
