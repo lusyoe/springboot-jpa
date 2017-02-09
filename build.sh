2872872ae546361097b7bd806eeaac1694d56619
lusyoe <lusyoe@163.com>
2017-02-09 15:30:09 +0800

    in the stop container before run

--- build.sh
+++ build.sh
@@ -2,9 +2,14 @@
 
 source /etc/profile
 
+MYIMAGE=192.168.1.2:8082/springboot/springboot-jpa
+
 docker login 192.168.1.2:8082 -u admin -p admin123
 
 mvn package -e -X docker:build -DskipTest
 
-docker run -dp 9090:8080 192.168.1.2:8082/springboot/springboot-jpa
+docker stop ${MYIMAGE}
+
+docker run -dp 9090:8080 --name springboot-jpa ${IMAGE}
+
 
